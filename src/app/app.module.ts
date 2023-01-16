bdb3bba4241f28cc97669799ee04f87bd887a1d5
dharakachhadia <dharakachhadia@gmail.com>
2023-01-16 18:13:27 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, MatTableModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  public getJsonValue: any;
+  public displayColumns: string[] = ['title', 'website', 'submission_address'];
+  public dataSource: any = [];
+  counter: number = 0;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getMethod(this.counter);
+  }
+  public getMethod(offset: number) {
+    this.http
+      .get(
+        `https://api.foia.gov/api/agency_components?filter%5Bstatus%5D=1\u0026page%5Boffset%5D=${offset}\u0026page%5Blimit%5D=10&fields[agency_component]=title,abbreviation,website,submission_address&api_key=QWAVKOCTHg4QuCD38A7hwDCq3p2OakcSfdbpQyIW`
+      )
+      .subscribe((data: any) => {
+        console.log(data.data);
+        this.getJsonValue = data.data;
+        this.dataSource = data.data;
+      });
+  }
+
+  public next() {
+    this.getMethod(this.counter);
+    this.counter = this.counter + 10;
+  }
+}
